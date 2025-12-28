079d3975f1ab8158574d4fbda8fdf62b10eca62a
houriamir <houriamir@ump.ac.ma>
2025-12-28 15:15:28 +0000

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from main import greet
 
 def test_greet(capsys):
     greet("Alice")
     captured = capsys.readouterr()
     assert captured.out == "Bonjour, Alice!\n"
+
